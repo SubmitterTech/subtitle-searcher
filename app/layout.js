c64cf5128cce8482b6f668f7606d1ce733a9a3a9
Yunusemre Şentürk <se.yunusemre@gmail.com>
2025-04-09 01:03:31 +0300

    Disable fetching google fonts at behind the scenes
    Signed-off-by: Yunusemre Şentürk <se.yunusemre@gmail.com>

--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,25 @@
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata = {
   title: "Ses Ara",
-  description: "Written by SubmitterTech",
+  description: "Developed by SubmitterTech",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`--font-geist-sans`}>
         {children}
       </body>
     </html>
