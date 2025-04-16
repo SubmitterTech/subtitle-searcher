76e8d85424a3f376c3e3c6b37ab1192bb4e8f659
Yunusemre Şentürk <se.yunusemre@gmail.com>
2025-04-16 14:18:31 +0300

    Introduce static manifest file
    Signed-off-by: Yunusemre Şentürk <se.yunusemre@gmail.com>

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,4 @@
-// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata = {
   title: "Ses Ara",
@@ -19,7 +8,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`--font-geist-sans`}>
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <link rel="apple-touch-icon" href="/logo.png" />
+      </head>
+      <body className="--font-geist-sans">
         {children}
       </body>
     </html>
